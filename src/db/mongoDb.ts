bf28ab65d8e25ade0fd7ede22a7833166be0c805
Gyorgy Gabor <gyorgyg@gmail.com>
2023-11-22 18:02:53 +0100

    feat: connect to mongodb

---
+++ src/db/mongoDb.ts
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+
+const url = 'mongodb://127.0.0.1:27017/vineStoreDb'
+
+mongoose.connect(url)
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error)
+  })
