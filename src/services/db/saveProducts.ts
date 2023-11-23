d68ed9296f2d4c6331e73a66e8ee6e90f12d42be
Gyorgy Gabor <gyorgyg@gmail.com>
2023-11-23 12:12:22 +0100

    feat(saveProducts): save Producer

--- src/services/db/saveProducts.ts
+++ src/services/db/saveProducts.ts
@@ -1,11 +1,16 @@
 import { Product } from '@src/db/models'
 import type { ProductType } from '@src/types'
-import findProducerByName from './findProducerByName'
+import getProducer from './getProducer'
 import type { IProduct } from '@src/db/models/Product'
+import saveProducer from './saveProducer'
 
 const saveProducts = async (products: ProductType[]): Promise<IProduct[]> => {
   const updatedProducts = await Promise.all(products.map(async (product) => {
-    const producer = await findProducerByName(product.producer)
+    let producer = await getProducer(product.producer)
+
+    if (!producer) {
+      producer = await saveProducer(product.producer)
+    }
 
     return {
       name: product.name,
