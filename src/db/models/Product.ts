2055be164e6ab7b7edb23d4d9644b318dfccb489
Gyorgy Gabor <gyorgyg@gmail.com>
2023-11-23 14:25:12 +0100

    feat(Product model): add alias to the schema

--- src/db/models/Product.ts
+++ src/db/models/Product.ts
@@ -10,7 +10,7 @@ export interface IProduct extends Document {
 const ProductSchema: Schema = new Schema({
   name: { type: String, required: true },
   vintage: { type: String, required: true },
-  producerId: { type: mongoose.Types.ObjectId, required: true, ref: 'Producer' }
+  producerId: { type: mongoose.Types.ObjectId, required: true, ref: 'Producer', alias: 'producer' }
 })
 
 const Product = mongoose.model<IProduct>('Product', ProductSchema)
