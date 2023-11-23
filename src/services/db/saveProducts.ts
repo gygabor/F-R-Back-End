6e94276d16b1341c26bfc3e522c5ff984da42338
Gyorgy Gabor <gyorgyg@gmail.com>
2023-11-23 18:53:45 +0100

    refactor(saveProducts): delete return

--- src/services/db/saveProducts.ts
+++ src/services/db/saveProducts.ts
@@ -1,10 +1,9 @@
 import { Product } from '@src/db/models'
 import type { ProductType } from '@src/types'
 import getProducer from './getProducer'
-import type { IProduct } from '@src/db/models/Product'
 import saveProducer from './saveProducer'
 
-const saveProducts = async (products: ProductType[]): Promise<IProduct[]> => {
+const saveProducts = async (products: ProductType[]): Promise<void> => {
   const updatedProducts = await Promise.all(products.map(async (product) => {
     let producer = await getProducer(product.producer)
 
@@ -19,7 +18,7 @@ const saveProducts = async (products: ProductType[]): Promise<IProduct[]> => {
     }
   }))
 
-  return await Product.create(updatedProducts)
+  await Product.create(updatedProducts)
 }
 
 export default saveProducts
