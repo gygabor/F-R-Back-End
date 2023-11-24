f2299eb7985349978e1893479006edd997912188
Gyorgy Gabor <gyorgyg@gmail.com>
2023-11-24 08:39:11 +0100

    feat: use dotenv for mongodb url

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import 'module-alias/register'
+import 'dotenv/config'
 import app from './app'
 import { port } from './constants'
+
+console.log(process.env.MONGO_DB)
 
 app.listen(port, () => {
   console.log('Server running on port ' + port + '!')
