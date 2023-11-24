05222fd4b530e9ac6b929dd6c590b4e267286e01
Gyorgy Gabor <gyorgyg@gmail.com>
2023-11-24 07:53:41 +0100

    feat(fetchCsv): upsert Production, error handling

--- src/services/csv/fetchCsv.ts
+++ src/services/csv/fetchCsv.ts
@@ -1,17 +1,32 @@
-import { Transform } from 'stream'
+import csv from 'csv-parser'
 import getCsvStream from './getCsvStream'
-
-const group = new Transform({
-  transform: (chunk, encoding, callback) => {
-    console.log(chunk)
-    callback(null, chunk)
-  }
-})
+import type { ProductType } from '@src/types'
+import parseProduct from './parseProduct'
+import { upsert } from '../db'
 
 const fetchCsv = async (url: string): Promise<void> => {
+  let products: ProductType[] = []
+
   const csvStream = await getCsvStream(url)
+
   csvStream
-    .pipe(group)
+    .pipe(csv())
+    .on('data', async (data: any) => {
+      products.push(parseProduct(data))
+      if (products.length === 100) {
+        csvStream.pause()
+        await upsert(products)
+        products = []
+        csvStream.resume()
+      }
+    })
+    .on('error', (error: Error) => {
+      console.log('Error while reading file.')
+      console.error(error)
+    })
+    .on('end', () => {
+      console.log('Finished reading file.')
+    })
 }
 
 export default fetchCsv
