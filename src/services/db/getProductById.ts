81fd7b9701a2fcb983944ab333c6b637db94dd99
Gyorgy Gabor <gyorgyg@gmail.com>
2023-12-06 14:50:14 +0100

    refactor(db services): rename getProduct

--- src/services/db/getProductById.ts
+++ src/services/db/getProductById.ts
@@ -1,9 +1,9 @@
 import { Product } from '@src/db/models'
 import type { IProduct } from '@src/db/models/Product'
 
-const getProduct = async (_id: string): Promise<IProduct | null> => {
+const getProductById = async (_id: string): Promise<IProduct | null> => {
   const product = await Product.findById(_id).populate({ path: 'producerId' })
   return product
 }
 
-export default getProduct
+export default getProductById
