bfa618403dd25e35022b1a4edfff116182395db6
Gyorgy Gabor <gyorgyg@gmail.com>
2023-12-08 20:08:55 +0100

    fix(createProducts): return value

--- src/services/products/createProducts.ts
+++ src/services/products/createProducts.ts
@@ -1,30 +1,36 @@
 import {
   getProducers,
-  upsertProducers,
+  updateProducers,
   updateProducts,
   getProductById 
 } from '@src/services/db'
 import { ProductType } from './types'
 import getCreateProductsOperations from './getCreateProductsOperations'
+import { UpdatedProductType } from '@src/types'
 
-const createProducts = async (products: ProductType[]) => {
+const createProducts = async (products: ProductType[]): Promise<UpdatedProductType[]> => {
   const producers = products.map((product) => product.producer)
 
-  await upsertProducers(producers)
+  await updateProducers(producers)
 
   const producersAll = await getProducers()
   const operations = getCreateProductsOperations(products, producersAll)
   
   const result = await updateProducts(operations)
 
-  const insertedProducts = await Promise.all(Object.values(result.insertedIds).map(async (id) => await getProductById(id)))
-
-  return insertedProducts.map((product) => ({
-    _id: product?._id,
-    name: product?.name,
-    vintage: product?.vintage,
-    producer: product?.producerId,
+  const insertedProducts = await Promise.all(Object.values(result.insertedIds).reduce(async (acc, id) => {
+    const product = await getProductById(id)
+    if (product) {
+      acc = [...acc, {
+        _id: product?._id,
+        name: product?.name,
+        vintage: product?.vintage,
+        producer: product?.producerId,
+      }]
+    }
   }))
+
+  return insertedProducts
 }
 
 export default createProducts
