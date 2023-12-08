6a7d17a239afec0b140b5eab6b9b4c39c2465359
Gyorgy Gabor <gyorgyg@gmail.com>
2023-12-08 20:05:04 +0100

    refactor(getProducById): return product

--- src/services/db/getProductById.ts
+++ src/services/db/getProductById.ts
@@ -1,9 +1,9 @@
 import { Product } from '@src/db/models'
 import type { IProduct } from '@src/db/models/Product'
+import type { Types } from 'mongoose'
 
-const getProductById = async (_id: string): Promise<IProduct | null> => {
-  const product = await Product.findById(_id).populate({ path: 'producerId' })
-  return product
-}
+const getProductById = async (_id: string): Promise<IProduct | null> => (
+  await Product.findById(_id).populate({ path: 'producerId' })
+)
 
 export default getProductById
