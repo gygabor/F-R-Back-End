393540f053eb42babb2184456d199fef70291a52
Gyorgy Gabor <gyorgyg@gmail.com>
2023-12-08 20:03:39 +0100

    refactor(db service): delete unused services

--- src/services/db/getProducerById.ts
+++
@@ -1,8 +0,0 @@
-import { Producer } from '@src/db/models'
-import type { IProducer } from '@src/db/models/Producer'
-
-const getProducerById = async (id: string): Promise<IProducer | null> => {
-  return await Producer.findById(id)
-}
-
-export default getProducerById
